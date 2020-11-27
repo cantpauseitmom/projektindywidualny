a6cfd39ebc718e9bb013fd0c6bd59a99b936b527
hfelicjaniak <hfelicjaniak@edu.cdv.pl>
2020-11-27 19:50:04 +0100

    finalna wersja

---
+++ Assets/scripts/KillMobs.cs
@@ -0,0 +1,15 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class KillMobs : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(( Input.GetMouseButton(0)) && (other.GameObject.CompareTag("Enemy") ))
+            {
+                Destroy(other.GameObject);
+            }
+    }
+}*/
---
+++ Assets/scripts/SpriteChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChange : MonoBehaviour
+{
+
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>(); 
+    }
+
+    public void change(Sprite differentSprite)
+    {
+        spriteRenderer.sprite = differentSprite;
+    }
+}
